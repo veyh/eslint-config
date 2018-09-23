e4a144a484e38ee51a855da1e9cc207e89c0ae3b
wibe <wibestn@gmail.com>
2018-09-23 14:50:04 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "indent": [2, 2, {
+      "SwitchCase": 1,
+      "MemberExpression": "off",
+      "FunctionDeclaration": { "parameters": "first" },
+      "FunctionExpression": { "parameters": "first" },
+    }],
+    "linebreak-style": [2, "unix"],
+    "quotes": [2, "double"],
+    "semi": [2, "always"],
+    "no-console": [1],
+    "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    "no-constant-condition": [0],
+    "no-restricted-globals": [2, "Promise"],
+    // "one-var": [2, "always"],
+    "no-case-declarations": [2],
+    "no-unneeded-ternary": [2],
+    "space-before-blocks": [2, "always"],
+    "keyword-spacing": [2],
+    "arrow-spacing": [2],
+    "space-infix-ops": [2],
+    "eol-last": [2, "always"],
+    "newline-per-chained-call": [2],
+    "no-whitespace-before-property": [2],
+    "padded-blocks": [2, "never"],
+    "space-in-parens": [2, "never"],
+    "array-bracket-spacing": [2, "never"],
+    "object-curly-spacing": [2, "always"],
+    "max-len": [2, { "code": 80, "ignoreStrings": true }],
+    "comma-style": [2, "last"],
+    "comma-dangle": [2, {
+      "arrays": "only-multiline",
+      "objects": "only-multiline",
+      "imports": "only-multiline",
+      "exports": "only-multiline",
+      "functions": "ignore", // TODO "only-multiline"
+    }],
+    "new-cap": [2, { properties: false }],
+    "no-useless-escape": [2],
+    "no-tabs": [2],
+  },
+  "overrides": [{
+    files: ["**/*.spec.js"],
+    env: {
+      mocha: true,
+    },
+  }]
+};
